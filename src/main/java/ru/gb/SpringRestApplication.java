b9b570aee4c4fc7342204b1de0ff8e2b3db0bdc4
MarishaZ <Marisha.DL@yandex.ru>
2022-05-10 12:12:25 +0300

    part 9

--- src/main/java/ru/gb/SpringRestApplication.java
+++ src/main/java/ru/gb/SpringRestApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringRestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDataApplication.class, args);
+        SpringApplication.run(SpringRestApplication.class, args);
     }
 }
--- src/main/java/ru/gb/persistence/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.gb.persistence.repositories;
-
-import ru.gb.persistence.entities.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
